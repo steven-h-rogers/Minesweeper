23aef6d39a50fc922f4b2207acea9824940fb5bf
Steven Rogers <s.hunter.rogers@gmail.com>
2026-01-14 00:50:14 -0500

    sanity commit 2

--- src/AtlasManager.py
+++ src/AtlasManager.py
@@ -1,24 +1,29 @@
 import pygame
 class AtlasManager:
     GAME_ASSETS = None
-
+    #This line will cause an issue as pygame has not yet been initialized. Need to use a constructor instead
     ATLAS = pygame.image.load('graphics/minesweeper_spritesheet.png').convert_alpha()
 
-    # Dimensions are hardcoded into the spritesheet
+    # Dimensions, order, and sizes are hardcoded into the spritesheet
     ATLAS_ROWS = 2
     ATLAS_COLS = 8
-    ATLAS_COORDS = {} # Stores the symbol and coords data necessary for a blit into memory
     TILES = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '!', 'X', '*', ' ', '<', '?'] # All possible tiles
+    TILE_SIZE = 32
 
 
 
-    def initialize_atlas_coords_dict(self):
+    def load_tiles_to_memory():
         tile_index = 0
-        for i in range(self.ATLAS_ROWS):
-            for j in range(self.ATLAS_COLS):
+        for i in range(AtlasManager.ATLAS_ROWS):
+            for j in range(AtlasManager.ATLAS_COLS):
                 if tile_index < 15:
-                    curr_symbol = self.TILES[tile_index]
-                    curr_symbol_coord_data = (j*self.TILE_SIZE, i*self.TILE_SIZE, self.TILE_SIZE, self.TILE_SIZE)
+                    curr_symbol = AtlasManager.TILES[tile_index]
+                    curr_symbol_coord_data = (j*AtlasManager.TILE_SIZE, i*AtlasManager.TILE_SIZE, AtlasManager.TILE_SIZE, AtlasManager.TILE_SIZE)
                     curr_symbol_rect = pygame.Rect(curr_symbol_coord_data)
-                    self.ATLAS_COORDS[curr_symbol] = curr_symbol_rect
+                    AtlasManager.GAME_ASSETS[curr_symbol] = pygame.Surface.subsurface(curr_symbol_rect)
                     tile_index += 1
+
+    def get_Game_Assets():
+        return AtlasManager.GAME_ASSETS
+
+    load_tiles_to_memory()
--- src/Game.py
+++ src/Game.py
@@ -2,6 +2,7 @@ import pygame
 import Tile
 import random
 import numpy as np
+import AtlasManager
 
 """win condition is once all bombs are flagged and all non bombs are revealed
 rows = y axis
@@ -27,20 +28,12 @@ class Game:
     # Dimensions are baked into the spritesheet
     ATLAS_ROWS = 2
     ATLAS_COLS = 8
-    ATLAS_COORDS = {} # Stores the symbol and coords data necessary for a blit into memory
 
     TILE_SIZE = 32 # Each tile in the sheet is 32x32
     TILES = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '!', 'X', '*', ' ', '<', '?'] # All possible tiles
-    ALWAYS_NECESSARY_TILES = ['!', ' ', '<', '?', 'X', '*'] # Tiles that will be needed/can be displayed if the user wishes uses them
 
     def __init__(self, dimensions, num_bombs):
-        """TODO: In the future, this class should be split into two classes so that 
-        image doesn't have to be loaded upon every new game. Have a seperate class 
-        that loads all of the things that will always be needed into memory once 
-        and then another one that handles all of the things that are unique to the 
-        actual game instance"""
-        self.ATLAS = pygame.image.load('graphics/minesweeper_spritesheet.png').convert_alpha() # Load Spritesheet on the class level
-
+        self.ATLAS = AtlasManager.
         self.dimensions = dimensions
         self.cols, self.rows = self.dimensions
 
@@ -48,12 +41,7 @@ class Game:
         self.bomb_probability = num_bombs/(self.rows*self.cols)
 
         self.bomb_map = self.initialize_bomb_map(self.rows, self.cols, self.bomb_probability)
-        self.curr_game_tiles = self.get_unique_tiles(self.bomb_map)
-        self.curr_game_tiles.update(Game.ALWAYS_NECESSARY_TILES)
-        self.initialize_atlas_coords_dict()
 
-        self.display_dict(Game.ATLAS_COORDS)
-        # self.curr_game_tile_dict = self.load_game_tiles_to_memory()
 
 
     """generatre a 2d array that is only responsible for placing the bombs"""
@@ -70,7 +58,7 @@ class Game:
             for j in range(cols):
                 if bomb_map[i][j] == True:
                     bomb_map[i][j] = '!'
-                else:
+                else: #TODO: maybe make this function a bit more single responsibility compatible and have the checking of tiles live in it's own function to improve readability
                     adjacent_bombs = 0
                     for direction in Game.CHECK_DIRECTIONS.values():
                         col_dir = direction[0]
@@ -85,35 +73,11 @@ class Game:
     def display_bomb_map(self, bomb_map):
         for row in bomb_map:
             print(row)
-
-    def get_unique_tiles(self, bomb_map):
-        unique_tiles = set()
-        for row in bomb_map:
-            unique_tiles.update(row)
-        unique_tiles.discard('!')
-        return unique_tiles
     
-
-    def initialize_atlas_coords_dict(self):
-        tile_index = 0
-        for i in range(self.ATLAS_ROWS):
-            for j in range(self.ATLAS_COLS):
-                if tile_index < 15:
-                    curr_symbol = self.TILES[tile_index]
-                    curr_symbol_coord_data = (j*self.TILE_SIZE, i*self.TILE_SIZE, self.TILE_SIZE, self.TILE_SIZE)
-                    curr_symbol_rect = pygame.Rect(curr_symbol_coord_data)
-                    self.ATLAS_COORDS[curr_symbol] = curr_symbol_rect
-                    tile_index += 1
-
-
     def display_dict(self, dict_to_print):
         for k,v in dict_to_print.items():
             print(k,v,sep=': ')
     
 
-    def load_game_tiles_to_memory(self):
-        curr_game_surfaces = {}
-        for symbol in self.curr_game_tiles:
-           curr_game_surfaces[symbol] = self.ATLAS.subsurface(Game.ATLAS_COORDS[symbol])
-        return curr_game_surfaces
+
             
