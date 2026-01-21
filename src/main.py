990d320e5c01913f1de239127132c4c6277431a8
Steven Rogers <s.hunter.rogers@gmail.com>
2026-01-21 17:43:49 -0500

    fit and centered grid on a fullscreen window

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import pygame, sys
 from Game import Game
 
 pygame.init()
-screen = pygame.display.set_mode((32*50,32*50), pygame.RESIZABLE)
+screen = pygame.display.set_mode((50*32,50*32), pygame.SCALED | pygame.FULLSCREEN)
 clock = pygame.time.Clock()
 game = Game((50,50), 175, screen)
 
